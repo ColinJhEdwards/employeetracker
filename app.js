bbbbcea625b05743e98f99b2bd84d4472dccff52
Colin Edwards <ColinJhEdwards@gmail.com>
2021-03-12 20:20:48 -0800

    set up mqsql and inquirer

---
+++ app.js
@@ -0,0 +1,10 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "0550070@cjhe",
+  database: "top_songsDB",
+});
